3acf382e51b9a3b58f02aee80a5baaba3ecab31d
ImAgiNaTiOn <457707172@qq.com>
2019-02-01 18:27:29 +0800

    1
    1

--- plot_function.py
+++ plot_function.py
@@ -3,25 +3,25 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 offline.init_notebook_mode(connected=False)
 
-# 1. 使用最好看的画图的包的步骤
+# 1. 使用最好看的画图的包(plotly)的步骤
+def plotly_function(data, x='', y='', choose_clos=''):
+  cols_sample = data.choose_clos.sample(1).values[0]
+  print(f"the samples is:{cols_sample}!!!!!")
+  temp = data[data.choose_clos == cols_sample].reset_index(drop=True)
+  trace0 = go.Scatter(x=temp[x].values.tolist(),
+                      y=temp[y].values.tolist(),
+                      name='dispatch',
+                      mode='lines+markers'
+                     )
+  layout = go.Layout(
+                    xaxis=dict(title=x),
+                    yaxis=dict(title=y)
+                    )
+  datas = go.Data([trace0])
+  fig = go.Figure(data=datas, layout=layout)
+  offline.iplot(fig, show_link=False)
 
-sampe = result.zone_sampe.sample(1).values[0]
-print(f"the zonecode is:{sampe}!!!")
-result_test = result[result.zone_sampe == sampe]
-# result_test
-
-trace0 = go.Scatter(x=result_test.columns.tolist()[1:-1],
-                    y=result_test.iloc[0,1:-1].values,
-                    name='dispatch',
-                    mode='lines+markers'
-)
-
-
-
-layout = go.Layout(
-                    xaxis=dict(title='日期'),
-                    yaxis=dict(title='派件时长')
-)
-data = go.Data([trace0])
-fig = go.Figure(data=data, layout=layout)
-offline.iplot(fig, show_link=False)
+# 2. 使用seabron进行画图的步骤
+  
+  
+  
